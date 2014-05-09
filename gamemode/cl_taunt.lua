7e27ea2841c94dd95ed3068fec22489c71d0350a
MechanicalMind <cyber_cam34@yahoo.com.au>
2014-05-09 23:58:44 +1000

    Taunt menu improvements

--- gamemode/cl_taunt.lua
+++ gamemode/cl_taunt.lua
@@ -14,6 +14,9 @@ end
 
 local function fillList(mlist, taunts)
 	menu.CurrentTaunts = taunts
+	for k, v in pairs(menu.CatList:GetCanvas():GetChildren()) do
+		v.Selected = false
+	end
 	mlist:Clear()
 	for k, t in pairs(taunts) do
 		if t.sex && t.sex != GAMEMODE.PlayerModelSex then
@@ -23,20 +26,20 @@ local function fillList(mlist, taunts)
 			continue
 		end
 		local but = vgui.Create("DButton")
-		but:SetTall(draw.GetFontHeight("RobotoHUD-15") * 1.4)
+		but:SetTall(draw.GetFontHeight("RobotoHUD-L15") * 1.0)
 		but:SetText("")
 		function but:Paint(w, h)
 			local col = Color(150, 150, 150)
 			if self:IsDown() then
 				colMul(col, 0.5)
 			elseif self:IsHovered() then
-				colMul(col, 1.2)
+				colMul(col, 1.4)
 			end
-			draw.RoundedBox(4, 0, 0, w, h, col)
+			-- draw.RoundedBox(4, 0, 0, w, h, col)
 		
 
-			draw.ShadowText(t.name, "RobotoHUD-15", 4, h / 2, Color(230, 230, 230), 0, 1)
-			draw.ShadowText(math.Round(t.soundDuration * 10) / 10 .. "s", "RobotoHUD-10", w - 4, h / 2, color_white, 2, 1)
+			draw.ShadowText(t.name, "RobotoHUD-L15", 0, h / 2, col, 0, 1)
+			draw.ShadowText(math.Round(t.soundDuration * 10) / 10 .. "s", "RobotoHUD-L10", w, h / 2, col, 2, 1)
 		end
 		function but:DoClick()
 			RunConsoleCommand("ph_taunt", t.sound[math.random(#t.sound)])
@@ -50,30 +53,36 @@ local function addCat(clist, name, taunts, mlist)
 	local z = tonumber(util.CRC(name):sub(1, 8))
 
 	local but = vgui.Create("DButton")
-	but:SetTall(draw.GetFontHeight("RobotoHUD-10") * 1.6)
+	but:SetTall(draw.GetFontHeight("RobotoHUD-15") * 1.3)
 	but:SetText("")
+	but.Selected = false
 	function but:Paint(w, h)
-		local col = Color(150, 150, 150)
-		if self:IsDown() then
-			colMul(col, 0.5)
-		elseif self:IsHovered() then
-			colMul(col, 1.2)
+		local col = Color(68, 68, 68, 160)
+		-- if self:IsDown() then
+		-- 	colMul(col, 0.5)
+		-- elseif self:IsHovered() then
+		-- 	colMul(col, 1.2)
+		-- end
+
+		if !self.Selected then
+			colMul(col, 0.7)
 		end
-		surface.SetDrawColor(col)
-		surface.DrawRect(0, 0, w, h)
 
+		draw.RoundedBoxEx(4, 0, 0, w, h, col, true, false, true, false)
 
-		draw.SimpleText(name, "RobotoHUD-10", w / 2, h / 2, Color(50, 50, 50), 1, 1)
+		draw.ShadowText(name, "RobotoHUD-15", w / 2, h / 2, color_white, 1, 1)
 	end
 	function but:DoClick()
 		fillList(mlist, taunts)
+		self.Selected = true
 	end
 	clist:AddItem(but)
+	return but
 end
 
 local function fillCats(clist, mlist)
 	clist:Clear()
-	addCat(clist, "All", Taunts, mlist)
+	local all = addCat(clist, "All", Taunts, mlist)
 	for k, taunts in pairs(TauntCategories) do
 		local c = 0
 		for a, t in pairs(taunts) do
@@ -86,12 +95,13 @@ local function fillCats(clist, mlist)
 			addCat(clist, k, taunts, mlist)
 		end
 	end
+	all.Selected = true
 end
 
 local function openTauntMenu()
 	if IsValid(menu) then
-		fillCats(menu.CatList, menu.TauntList)
 		fillList(menu.TauntList, menu.CurrentTaunts)
+		fillCats(menu.CatList, menu.TauntList)
 		menu:SetVisible(!menu:IsVisible())
 		return
 	end
@@ -108,7 +118,7 @@ local function openTauntMenu()
 	menu:DockPadding(8, 8 + draw.GetFontHeight("RobotoHUD-25"), 8, 8)
 
 	function menu:Paint(w, h)
-		surface.SetDrawColor(120, 120, 120)
+		surface.SetDrawColor(40,40,40,230)
 		surface.DrawRect(0, 0, w, h)
 
 		-- draw.ShadowText("Taunts", "RobotoHUD-15", 8, 4, color_white, 0)
@@ -124,21 +134,21 @@ local function openTauntMenu()
 	menu.CatList = clist
 	clist:Dock(LEFT)
 	clist:SetWide(menu:GetWide() * 0.3)
-	clist:DockMargin(0, 0, 4, 0)
+	clist:DockMargin(0, 0, 0, 0)
 	local df = draw.GetFontHeight("RobotoHUD-15") * 1.6
 	function clist:Paint(w, h)
-		surface.SetDrawColor(60, 60, 60)
-		surface.DrawRect(0, 0, w, df)
+		-- surface.SetDrawColor(60, 60, 60)
+		-- surface.DrawRect(0, 0, w, df)
 
-		draw.SimpleText("Categories", "RobotoHUD-15", w / 2, df / 2, color_white, 1, 1)
+		-- draw.SimpleText("Categories", "RobotoHUD-15", w / 2, df / 2, color_white, 1, 1)
 	end
 
 	// child positioning
 	local canvas = clist:GetCanvas()
-	canvas:DockPadding(3, df, 3, 3)
+	canvas:DockPadding(0, 0, 0, 0)
 	function canvas:OnChildAdded( child )
 		child:Dock( TOP )
-		child:DockMargin(0, 0, 0, 1)
+		child:DockMargin(0, 0, 0, 4)
 	end
 
 
@@ -149,18 +159,24 @@ local function openTauntMenu()
 	function mlist:Paint(w, h)
 		surface.SetDrawColor(50, 50, 50)
 		-- surface.DrawRect(0, 0, w, h)
+
+		surface.SetDrawColor(68, 68, 68, 160)
+		surface.DrawOutlinedRect(0, 0, w, h)
+
+		surface.SetDrawColor(55, 55, 55, 120)
+		surface.DrawRect(1, 1, w - 2, h - 2)
 	end
 
 	// child positioning
 	local canvas = mlist:GetCanvas()
-	canvas:DockPadding(0, 0, 0, 0)
+	canvas:DockPadding(8, 8, 8, 8)
 	function canvas:OnChildAdded( child )
 		child:Dock( TOP )
-		child:DockMargin( 0,0,0,4 )
+		child:DockMargin(0, 0, 0, 4)
 	end
 
-	fillCats(clist, mlist)
 	fillList(mlist, Taunts)
+	fillCats(clist, mlist)
 end
 
 concommand.Add("ph_menu_taunt", openTauntMenu)
