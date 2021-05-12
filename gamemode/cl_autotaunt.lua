1c3226fbd8afc553c8dc213ea355bcad3de20593
James McNaughton <n347@newcastle.ac.uk>
2021-05-12 10:09:26 +0100

    Outlined work to do.

--- gamemode/cl_autotaunt.lua
+++ gamemode/cl_autotaunt.lua
@@ -1,10 +1,11 @@
---Taunt automatically three times a minute on average when a prop
-local frequency = 3
-local time = 60/(frequency * 2)
-timer.Create( "autotaunt", time, 0, function()
+--Taunt automatically every so often average once per so many seconds when a prop (number of seconds defined in the config) .
+local minTime = 10 -- // TODO Get time from config.
+local maxTime = 40 -- // TODO Get time from config.
+local time = math.random(minTime,maxTime)
+timer.Create( "autotaunt", 1, 0, function()
 	if LocalPlayer():Team() == 3 then
-		if math.random(1,2) == 1 then
-			RunConsoleCommand("ph_taunt_random") 
-		end
+		-- // TODO Check if player is taunting, if so reset the countdown.
+		RunConsoleCommand("ph_taunt_random") 
+		time = math.random(minTime,maxTime)
 	end
 end )
