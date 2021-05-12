315c7e8dc694060c0bf15627edb1adc2ca5ba73e
James McNaughton <n347@newcastle.ac.uk>
2021-05-12 10:26:53 +0100

    Reset automating taunt countdown when taunting.

--- gamemode/cl_autotaunt.lua
+++ gamemode/cl_autotaunt.lua
@@ -1,11 +1,18 @@
 --Taunt automatically every so often average once per so many seconds when a prop (number of seconds defined in the config) .
-local minTime = 10 -- // TODO Get time from config.
-local maxTime = 40 -- // TODO Get time from config.
+local minTime = 30 -- // TODO Get time from config.
+local maxTime = 60 -- // TODO Get time from config.
 local time = math.random(minTime,maxTime)
+local timeElapsed = 0
 timer.Create( "autotaunt", 1, 0, function()
 	if LocalPlayer():Team() == 3 then
-		-- // TODO Check if player is taunting, if so reset the countdown.
-		RunConsoleCommand("ph_taunt_random") 
-		time = math.random(minTime,maxTime)
+		if LocalPlayer().Taunting then
+			timeElapsed = 0
+		else
+			if timeElapsed > time then
+				RunConsoleCommand("ph_taunt_random") 
+				time = math.random(minTime,maxTime)
+			end
+		end
+		timeElapsed = timeElapsed + 1
 	end
 end )
